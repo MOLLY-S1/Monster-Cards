784c704739f9ad3a8312d03eaf3ed0d1f42bfb5a
MOLLY-S1 <sankeym@middleton.school.nz>
2023-04-27 19:53:13 +1200

    Update 01_Welcome_V3.py

--- 01_Welcome_V3.py
+++ 01_Welcome_V3.py
@@ -11,23 +11,23 @@ while True:
 
     # Enter user choice
     user_choice = easygui.buttonbox("Please choose an option:", "Menu Options",
-                                    choices=["Add Cards", "Search Catalogue",
+                                    choices=["Add Card", "Search Catalogue",
                                              "Delete Card", "Output Catalogue",
                                              "Exit"])
 
     # Output user choice
     if user_choice == "Add Card":
-        easygui.msgbox("Add Card", "Add Card")
+        print("Add Card")
 
     elif user_choice == "Search Catalogue":
-        easygui.msgbox("Search Catalogue", "Search Catalogue")
+        print("Search Catalogue")
 
     elif user_choice == "Delete Card":
-        easygui.msgbox("Delete Card", "Delete Card")
+        print("Delete Card")
 
     elif user_choice == "Output Catalogue":
-        easygui.msgbox("Output Catalogue", "Output Catalogue")
+        print("Output Catalogue")
 
     elif user_choice == "Exit":
-        easygui.msgbox("Goodbye", "Exit")
+        print("Goodbye")
         break
