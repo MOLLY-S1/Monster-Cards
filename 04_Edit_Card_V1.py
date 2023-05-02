e859d2dea33d93be8f3024df1dbcdd89bc3a0a2c
MOLLY-S1 <sankeym@middleton.school.nz>
2023-05-02 18:25:45 +1200

    Update 04_Edit_Card_V1.py

--- 04_Edit_Card_V1.py
+++ 04_Edit_Card_V1.py
@@ -3,26 +3,8 @@
 import easygui
 
 cards = {"Stoneling":
-             {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
-         "Vexscream":
-             {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
-         "Dawnmirage":
-             {"Strength": 5, "Speed": 15, "Stealth": 18, "Cunning": 22},
-         "Blazegolem":
-             {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
-         "Websnake":
-             {"Strength": 7, "Speed": 15, "Stealth": 10, "Cunning": 5},
-         "Moldvine":
-             {"Strength": 21, "Speed": 18, "Stealth": 14, "Cunning": 5},
-         "Vortexwing":
-             {"Strength": 19, "Speed": 13, "Stealth": 19, "Cunning": 2},
-         "Rotthing":
-             {"Strength": 16, "Speed": 7, "Stealth": 4, "Cunning": 12},
-         "Froststep":
-             {"Strength": 14, "Speed": 14, "Stealth": 17, "Cunning": 4},
-         "Wispghoul":
-             {"Strength": 17, "Speed": 19, "Stealth": 3, "Cunning": 2}
-         }
+             {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15}}
+
 
 while True:
     card = ""
@@ -63,15 +45,13 @@ while True:
                                          lowerbound=0, upperbound=25)
                 card[name][power] = new
 
-        if change_value == "Combo Name":
+        if change_value == "Card Name":
             new = easygui.enterbox(f"Enter the name you want to change "
-                                   f"{combo_ID} to:", "New Name").upper()
-            confirm_combo[new] = confirm_combo.pop(combo_ID)
-            print(confirm_combo)
+                                   f"{name} to:", "New Name").title()
+            cards[new] = cards.pop(name)
+            print(cards)
 
-        correct = easygui.buttonbox(f"Is the following combo correct?\n"
-                                    f"{ID}\n" f"{combo}", "Combo Check",
-                                    choices=["Yes", "No"])
+        correct = easygui.buttonbox(f"Is the following card correct?\n"
+                                f"{name}\n" f"{card}", "Card Check",
+                                choices=["Yes", "No"])
 
-change(combos)
-print(combos)
