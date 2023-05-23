40d95d68adf6d93352825729e92d5171309cea3c
MOLLY-S1 <sankeym@middleton.school.nz>
2023-05-24 09:16:11 +1200

    Base Update

--- 00_Base_V2.py
+++ 00_Base_V2.py
@@ -17,7 +17,7 @@ def welcome():
     return user_choice
 
 
-# Blank checker function2
+# Blank checker function
 def blank_check(question, title, box):
     error = "Please answer all questions"
     while True:
@@ -49,13 +49,15 @@ def blank_check(question, title, box):
 
 
 # Function to confirm and edit cards
-def edit(confirm_card):
+def edit(confirm_card, card_list):
     while True:
         card = ""
+        powers = ""
         for card_name, card_info in confirm_card.items():
             title = f"\nCard Name: {card_name}"
             for key, value in card_info.items():
                 card += f"{key}: {value}\n"
+                powers += f"-{key}-"
 
         correct = easygui.buttonbox(f"Is the following card correct?\n"
                                     f"{title}\n" f"{card}", "Card Check",
@@ -80,13 +82,14 @@ def edit(confirm_card):
 
             while current_value not in confirm_card[card_name]:
                 easygui.msgbox("Sorry, that is not the name of "
-                               "an power in this card:\n\n"
+                               "a power in this card:\n\n"
                                f"{title}\n" f"{card}", "Error")
 
-                current_value = blank_check("Enter the name of the "
+                current_value = blank_check(f"Enter the name of the "
                                             "power which value you wish "
-                                            "to change:", "Power Name",
-                                            "enter").title()
+                                            "to change:\n"
+                                            f"The options are: {powers} ",
+                                            "Power Name", "enter").title()
 
             new = blank_check(f"Enter the value you want to change"
                               f" {current_value} to:", "New Value",
@@ -97,7 +100,7 @@ def edit(confirm_card):
         elif change_value == "Card Name":
             new = blank_check(f"Enter the name you want to change "
                               f"{card_name} to:", "New Name", "enter").title()
-            if new in confirm_card:
+            if new in card_list:
                 new = blank_check(f"Sorry that name already is already taken!"
                                   f"Enter the name you want to change "
                                   f"{card_name} to:", "New Name",
@@ -111,7 +114,16 @@ def add_card(card_list):
     new_cards = {}
 
     # User enters power values
-    name = blank_check("Enter Card Name:", "Card Name", "enter")
+    name = blank_check("Enter Card Name:", "Card Name", "enter").title()
+
+    # Check if the entered name is already in the catalogue
+    if name in card_list:
+        easygui.msgbox(f"Sorry a card with the name {name} already exists in "
+                       f"this catalogue, please choose another name",
+                       "Error")
+        card_name = blank_check("Enter Card Name:", "Card Name", "enter"). \
+            title()
+
     strength = blank_check("Enter Strength Value:", "Strength",
                            "integer")
     speed = blank_check("Enter Speed Value:", "Speed", "integer")
@@ -125,7 +137,7 @@ def add_card(card_list):
     new_cards[name]["Stealth"] = stealth
     new_cards[name]["Cunning"] = cunning
 
-    correct_card = edit(new_cards)
+    correct_card = edit(new_cards, card_list)
     card_list.update(correct_card)
 
 
@@ -149,7 +161,7 @@ def search_card(card_list):
         searched_card = {search_name: card_list[search_name]}
 
         # Confirm the searched card
-        correct_card = edit(searched_card)
+        correct_card = edit(searched_card, card_list)
 
         # Update the catalogue
         del [card_list[search_name]]
@@ -203,7 +215,7 @@ def output_catalogue(card_list):
 # MAIN ROUTINE
 
 catalogue = {"Stoneling":
-             {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
+                 {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
              "Vexscream":
                  {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
              "Dawnmirage":
