13df2ef160da2ff6c7ada81b60d99f964448b501
MOLLY-S1 <sankeym@middleton.school.nz>
2023-05-28 14:58:31 +1200

    Update 00_Base_Final.py

--- 00_Base_Final.py
+++ 00_Base_Final.py
@@ -33,6 +33,7 @@ def welcome():
 
 # Blank checker function
 def blank_check(question, title, box):
+    # Error statement
     error = "Please answer all questions"
     while True:
         # Enter-box
@@ -40,6 +41,7 @@ def blank_check(question, title, box):
             # Ask for input
             response = easygui.enterbox(question, title)
 
+            # If nothing is entered
             if response == "":
                 easygui.msgbox(error, "Error")
 
@@ -72,33 +74,39 @@ def edit(confirm_card, card_list):
         powers = ""
         card_name = ""
         title = ""
+        # Print card information
         for card_name, card_info in confirm_card.items():
             title = f"\nCard Name: {card_name}"
             for key, value in card_info.items():
                 card += f"{key}: {value}\n"
                 powers += f"-{key}-"
 
+        # Check if correct
         correct = easygui.buttonbox(f"Is the following card correct?\n"
                                     f"{title}\n" f"{card}", "Card Check",
                                     choices=["Yes", "No"])
 
+        # If correct add card to catalogue
         if correct == "Yes":
             easygui.msgbox(f"You have successfully entered the new card "
                            f"{card_name} to the Monster Cards Catalogue",
                            "Card added")
             return confirm_card
 
+        # Edit card info
         change_value = easygui.buttonbox("What would you like to change?",
                                          "Change Choice",
                                          choices=["Power Value",
                                                   "Card Name", ])
 
+        # Edit power value
         if change_value == "Power Value":
             current_value = blank_check("Enter the name of the power "
                                         "which value you wish to "
                                         "change:", "Power Name",
                                         "enter").title()
 
+            # If invalid power is entered
             while current_value not in confirm_card[card_name]:
                 easygui.msgbox("Sorry, that is not the name of "
                                "a power in this card:\n\n"
@@ -110,20 +118,27 @@ def edit(confirm_card, card_list):
                                             f"The options are: {powers} ",
                                             "Power Name", "enter").title()
 
+            # Enter new value
             new = blank_check(f"Enter the value you want to change"
                               f" {current_value} to:", "New Value",
                               "integer")
 
+            # Update Card
             confirm_card[card_name][current_value] = new
 
+        # Edit card name
         elif change_value == "Card Name":
             new = blank_check(f"Enter the name you want to change "
                               f"{card_name} to:", "New Name", "enter").title()
+
+            # Ensure it is a unique name
             if new in card_list:
                 new = blank_check(f"Sorry that name already is already taken!"
-                                  f"Enter the name you want to change "
+                                  f" Enter the name you want to change "
                                   f"{card_name} to:", "New Name",
                                   "enter").title()
+
+            # Update Card info
             confirm_card[new] = confirm_card.pop(card_name)
 
 
@@ -144,6 +159,7 @@ def add_card(card_list):
         name = blank_check("Enter Card Name:", "Card Name", "enter"). \
             title()
 
+    # Enter the value of each power
     strength = blank_check("Enter Strength Value:", "Strength",
                            "integer")
     speed = blank_check("Enter Speed Value:", "Speed", "integer")
@@ -157,7 +173,10 @@ def add_card(card_list):
     new_cards[name]["Stealth"] = stealth
     new_cards[name]["Cunning"] = cunning
 
+    # Confirm card is correct
     correct_card = edit(new_cards, card_list)
+
+    # Update correct card to card list and return to menu
     card_list.update(correct_card)
     welcome()
 
@@ -169,12 +188,17 @@ def search_card(card_list):
         search_name = blank_check("Enter name of card: ", "Search",
                                   "enter").title()
 
+        # If the name searched doesn't exist
         while search_name not in card_list:
             easygui.msgbox(
                 f"Sorry {search_name} is not a card in the Monster Card "
                 f"catalogue", "Card Not Found")
+
+            # Ask user to continue searching
             back = easygui.buttonbox("Would you like to continue searching?",
                                      "Continue?", choices=["Yes", "No"])
+
+            # If no return to menu
             if back == "No":
                 welcome()
 
@@ -188,7 +212,7 @@ def search_card(card_list):
         # Confirm the searched card
         correct_card = edit(searched_card, card_list)
 
-        # Update the catalogue
+        # Update the catalogue and return to menu
         del [card_list[search_name]]
         card_list.update(correct_card)
         welcome()
@@ -196,14 +220,18 @@ def search_card(card_list):
 
 # Function to delete cards from catalogue
 def delete_card(card_list):
+    # List of card names
     cards = []
     for card_name, card_info in card_list.items():
         # Card name added to list
         cards.append(card_name)
+
+    # Information to create choice-box
     text = "Enter any of the following cards to delete:\n"
     title = "Delete Card"
     delete = easygui.choicebox(text, title, cards)
 
+    # If cancel is pressed
     if not delete:
         welcome()
 
@@ -211,6 +239,8 @@ def delete_card(card_list):
     sure = easygui.buttonbox(f"Are you sure you want to delete {delete}\n"
                              f"Once it is deleted this cannot be undone",
                              "Delete Confirm", choices=["Yes", "No"])
+
+    # If confirmed remove card and return to menu
     if sure == "Yes":
         # Remove card from catalogue dictionary
         del [card_list[delete]]
@@ -218,6 +248,7 @@ def delete_card(card_list):
                        "Card Removed")
         welcome()
 
+    # Return to menu
     welcome()
 
 
@@ -237,11 +268,13 @@ def output_catalogue(card_list):
             # Card name and power values printed
             cards += f"{key}: {value} \n"
 
+    # Formatted output
     full_output = f"-----------------------\n" \
                   f"Monster Card Catalogue:\n" \
                   f"-----------------------\n" \
                   f"{cards}\n\n"
 
+    # Print full list to python console and return to menu
     print(full_output)
     welcome()
 
@@ -249,7 +282,7 @@ def output_catalogue(card_list):
 # MAIN ROUTINE
 
 catalogue = {"Stoneling":
-                 {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
+             {"Strength": 7, "Speed": 1, "Stealth": 25, "Cunning": 15},
              "Vexscream":
                  {"Strength": 1, "Speed": 6, "Stealth": 21, "Cunning": 19},
              "Dawnmirage":
@@ -275,4 +308,5 @@ easygui.msgbox("------------------------------------------------------------\n"
                "Welcome to the Monster Card Catalogue\n"
                "-------------------------------------------------------------",
                "Welcome")
+# Begin program
 welcome()
